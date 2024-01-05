c49d20ef80b552957dadf6f4a8c7a86fbbff607c
Justin <jjlon933@learner.dvusd.org>
2024-01-05 13:00:08 -0700

    Buttons
    Change Background colors

--- Menus/Form1.vb
+++ Menus/Form1.vb
@@ -1,3 +1,9 @@
 ﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        BackColor = Color.Red
+    End Sub
 
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        BackColor = Color.Green
+    End Sub
 End Class
